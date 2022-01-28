12d6cd9397e26171c262fe459e3ed0c01d9ea0e7
Gustavo Silva <gustavo.athenas13@gmail.com>
2022-01-27 22:31:26 -0300

    tipando arrays em ts

--- tipo-de-dados-ts/arrays.ts
+++ tipo-de-dados-ts/arrays.ts
@@ -1,2 +1,26 @@
+// array do tipo any
+let arrayAny: any = ["27", 27, true, {}]
+
+// array do tipo number
 let arrayNumbers:number[] = [1,2,3]
-console.log(arrayNumbers)
+arrayNumbers.push(4)
+
+// array do tipo string
+let arrayString:string[] = ["Gustavo", "kerolin"]
+
+// sintaxe de interface para fazer um array
+let arrayInterface: Array<string> = ["js","node ts","react ts"]
+
+// iterarando os arrays
+arrayNumbers.forEach(n => {
+    console.log(n)
+})
+for(let i of arrayString){
+    console.log(i)
+}
+for(let i =0; i < arrayInterface.length; i++){
+    console.log(arrayInterface[i])
+}
+arrayAny.forEach(element => {
+    console.log(element)
+});
